8e6f653ffdec6cb9d3c7455b0fd0098300e0cf6b
w0nd3r0us <steven.grewal@gmail.com>
2018-05-23 17:31:42 -0400

    completed ex 4.

--- exercise4.rb
+++ exercise4.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby -wKU
+def take_s(string)
+  if string.length < 8
+    return "false"
+  else
+    return "true"
+  end
+end
+
+puts "enter a word"
+
+string = gets.chomp
+
+puts take_s(string)
