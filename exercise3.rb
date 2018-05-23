85f55791f4e76958c0e868359cc09a6f247146ec
w0nd3r0us <steven.grewal@gmail.com>
2018-05-23 17:20:52 -0400

    completed ex 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby -wKU
+def is_even?(num)
+  if num % 2 == 0
+    return "even"
+  else
+    return "uneven"
+  end
+end
+
+puts "Is your number even?"
+
+num = gets.chomp.to_i
+
+puts is_even?(num)
