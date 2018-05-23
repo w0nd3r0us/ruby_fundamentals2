00044bfa9a792eea83a1848af92dde6c40dd957d
w0nd3r0us <steven.grewal@gmail.com>
2018-05-23 17:47:49 -0400

    finished ex 5.

--- exercise5.rb
+++ exercise5.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/env ruby -wKU
+def greet_backwards(name)
+  return "Hello, #{name.reverse}#{name.reverse}! Welcome home."
+end
+
+puts "please enter your name"
+name = gets.chomp
+puts(greet_backwards(name))
